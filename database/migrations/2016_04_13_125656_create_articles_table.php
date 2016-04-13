d34cb74a26b0e0014f1b747085366c56dd2627e0
cufrancis <cufrancis@163.com>
2016-04-13 21:06:43 +0800

    create Article model file

---
+++ database/migrations/2016_04_13_125656_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('sulg')->unique();
+            $table->string('title');
+            $table->text('content');
+            $table->integer('read')->default(0);
+            $table->integer('author');
+            $table->timestamps();
+            $table->timestamp('published_at')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('articles');
+    }
+}
